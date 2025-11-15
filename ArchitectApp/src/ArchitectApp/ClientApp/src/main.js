65168ecdf9f9b5cfa32bee0e01ba33af64ef7038
Balogh Károly <h252910@stud.u-szeged.hu>
2025-11-15 03:05:25 +0100

    feat: project structure workover, fe initial commit

---
+++ ArchitectApp/src/ArchitectApp/ClientApp/src/main.js
@@ -0,0 +1,7 @@
+﻿import $ from 'jquery';
+
+console.log('Message from main.js! Vite is running and jQuery is loaded.');
+
+$(document).ready(function () {
+    console.log('The DOM is loaded, jQuery is running.');
+});
