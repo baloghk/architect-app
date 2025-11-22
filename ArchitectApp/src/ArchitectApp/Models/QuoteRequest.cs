1610a1be122a3ee86a1e7ede47c102cb6d5682d8
baloghk <balogh2k4@gmail.com>
2025-11-22 03:12:31 +0100

    Release v0.2.0 - Db and Docker improvements (#3)
    * feat: add DbContext and qr model
    
    * ci: add docker-compose deployment with auto migration and port mapping

---
+++ ArchitectApp/src/ArchitectApp/Models/QuoteRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArchitectApp.Models
+{
+    public class QuoteRequest
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required, StringLength(100)]
+        public string ProjectType { get; set; }
+
+        [Required, StringLength(100)]
+        public string Location { get; set; }
+
+        [Required]
+        public string HousePlanPath { get; set; }
+
+        [Required]
+        public DateTime StartOfProject { get; set; }
+
+        [Required]
+        public DateTime EndOfProject { get; set; }
+
+        [Required, StringLength(2000)]
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
