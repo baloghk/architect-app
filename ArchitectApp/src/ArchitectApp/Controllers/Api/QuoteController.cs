6a2a4bb3aab2fd06ebaaf9305cc2de944fc22422
Balogh Károly <balogh2k4@gmail.com>
2025-11-23 18:35:28 +0100

    feat: add quotecontroller, restapi endpoints, swagger for dev testing

---
+++ ArchitectApp/src/ArchitectApp/Controllers/Api/QuoteController.cs
@@ -0,0 +1,43 @@
+﻿using ArchitectApp.Data;
+using ArchitectApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchitectApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+        private readonly ArchitectDbContext _context;
+
+        public QuoteController(ArchitectDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateQuote([FromBody] QuoteRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            request.CreatedAt = DateTime.UtcNow;
+
+            _context.QuoteRequests.Add(request);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Árajánlat sikeresen elküldve!", id = request.Id });
+
+            //TODO: email to admin
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.QuoteRequests.ToList());
+        }
+    }
+}
